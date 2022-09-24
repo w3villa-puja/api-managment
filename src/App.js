becc0e8f46bfe4f4ba4ae77d833ec7ece2f41ee7
w3villa-bedprakash <bedprakash.kushwaha@w3villa.com>
2022-09-24 23:37:05 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Registration from './Component/Registration';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Registration/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <Registration />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
